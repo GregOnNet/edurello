3baeceaa34e4433d8226268ad70596624d323dc0
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-09-14 20:48:19 +0200

    chore: install ngrx/effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {BackendService} from './backend.service';
+import { BackendService } from './backend.service';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot(reducers, {
@@ -17,11 +16,10 @@ import { reducers, metaReducers } from './reducers';
         strictStateImmutability: true,
         strictActionImmutability: true
       }
-    })
+    }),
+    EffectsModule.forRoot([])
   ],
   providers: [BackendService],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-
-}
+export class AppModule {}
